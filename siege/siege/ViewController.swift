89fa7d6124e74652bd7245fd26564be7037e3b88
Wei Wei <jheng.wei0919@gmail.com>
2020-10-22 18:36:50 +0800

    commit

---
+++ siege/siege/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  siege
+//
+//  Created by oneplay on 2020/10/22.
+//  Copyright © 2020 JackPan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
