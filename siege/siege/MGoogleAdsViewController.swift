3a463854fb016a8561c0370c71f0a32e7d9dfdae
Wei Wei <jheng.wei0919@gmail.com>
2020-10-26 14:16:09 +0800

    加入 廣告

---
+++ siege/siege/MGoogleAdsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  MGoogleAdsViewController.swift
+//  siege
+//
+//  Created by oneplay on 2020/10/26.
+//  Copyright © 2020 JackPan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMobileAds
+class MGooogleAdsViewController: UIViewController ,GADBannerViewDelegate{
+    var adBannerView: GADBannerView!
+
+    override func viewDidLoad() {
+        
+        setAdBanner()
+
+    }
+    
+    
+    func setAdBanner(){
+        let id = "ca-app-pub-7019441527375550/6672424835"
+        adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+        adBannerView!.adUnitID = id
+        adBannerView!.delegate = self
+        adBannerView!.rootViewController = self
+        adBannerView!.load(GADRequest())
+    }
+    
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        addBannerViewToView(bannerView)
+        
+    }
+    
+    // Called when an ad request failed.
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print((error.localizedDescription))
+    }
+    
+    // Called just before presenting the user a full screen view, such as a browser, in response to
+    // clicking on an ad.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    
+    // Called just before dismissing a full screen view.
+    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    
+    // Called just after dismissing a full screen view.
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    
+    // Called just before the application will background or terminate because the user clicked on an
+    // ad that will launch another application (such as the App Store).
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+        print(#function)
+    }
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: bottomLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+        ])
+    }
+
+}
--- siege/siege/ViewController.swift
+++ siege/siege/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: MGooogleAdsViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
