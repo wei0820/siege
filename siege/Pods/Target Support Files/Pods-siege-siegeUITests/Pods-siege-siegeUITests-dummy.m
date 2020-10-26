0e22a49d40c1743f0d357aedbcae8cc014994535
Wei Wei <jheng.wei0919@gmail.com>
2020-10-26 13:57:26 +0800

    add icon

---
+++ siege/Pods/Target Support Files/Pods-siege-siegeUITests/Pods-siege-siegeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_siege_siegeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_siege_siegeUITests
+@end
---
+++ siege/Pods/Target Support Files/Pods-siege/Pods-siege-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_siege : NSObject
+@end
+@implementation PodsDummy_Pods_siege
+@end
