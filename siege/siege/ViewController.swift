3fff4d75eead9566a8e6f140575d21027442b9f2
Wei Wei <jheng.wei0919@gmail.com>
2020-10-30 11:24:33 +0800

    add mapbox lib

--- siege/siege/ViewController.swift
+++ siege/siege/ViewController.swift
@@ -13,9 +13,43 @@ class ViewController: MGooogleAdsViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLeftButton(s: "會員中心")
+        setRightButton(s: "地圖")
+    
+        
+            
+    }
+    func setRightButton(s: String){
+        // 導覽列右邊按鈕
+        
+        let LeftButton = UIBarButtonItem(
+            title:s,
+            style:.plain,
+            target:self,
+            action:#selector(ViewController.setting))
+        // 加到導覽列中
+        self.navigationItem.leftBarButtonItem = LeftButton
+    }
+    @objc func setting() {
         
     }
-
+    
+    
+    func setLeftButton(s: String){
+        // 導覽列右邊按鈕
+        
+        let rightButton = UIBarButtonItem(
+            title:s,
+            style:.plain,
+            target:self,
+            action:#selector(ViewController.checkIn))
+        // 加到導覽列中
+        self.navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    @objc func checkIn() {
+        
+    }
 
 }
 
