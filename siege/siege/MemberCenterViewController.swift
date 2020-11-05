bc6eb2da6aa1e919d42afd4eaf8d1dc179193e41
Wei Wei <jheng.wei0919@gmail.com>
2020-11-05 15:06:36 +0800

    add memberCenter

---
+++ siege/siege/MemberCenterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MemberCenterViewController.swift
+//  siege
+//
+//  Created by oneplay on 2020/11/5.
+//  Copyright © 2020 JackPan. All rights reserved.
+//
+
+import UIKit
+
+class MemberCenterViewController: MGooogleAdsViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
