438097eaf31e83f8cce5b84c1760bd64c956070b
Wei Wei <jheng.wei0919@gmail.com>
2020-11-11 11:40:44 +0800

    新增註冊

--- siege/siege/MemberCenterViewController.swift
+++ siege/siege/MemberCenterViewController.swift
@@ -25,4 +25,40 @@ class MemberCenterViewController: MGooogleAdsViewController {
         isNoLoginView.isHidden = true
     }
     
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        let signUpAlert = UIAlertController(title: "註冊", message: "建立一個新帳戶", preferredStyle: .alert)
+               let saveAction = UIAlertAction(title: "創建", style: .default) { (action) in
+                   let emailSignUpTextField = signUpAlert.textFields![0]
+                   let passwordSignUpTextField = signUpAlert.textFields![1]
+                   Auth.auth().createUser(withEmail: emailSignUpTextField.text!, password: passwordSignUpTextField.text!, completion: { (user, error) in
+                       if error == nil {
+                           Auth.auth().signIn(withEmail: emailSignUpTextField.text!, password: passwordSignUpTextField.text!, completion: nil)
+                       } else {
+                           let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                           let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                           alert.addAction(defaultAction)
+                           self.present(alert, animated: true, completion: nil)
+                       }
+                   })
+               }
+               
+               let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+               
+               signUpAlert.addTextField { (emailSignUpTextField) in
+                   emailSignUpTextField.placeholder = "請輸入Email"
+               }
+               signUpAlert.addTextField { (passwordSignUpTextField) in
+                   passwordSignUpTextField.placeholder = "請輸入密碼"
+                   passwordSignUpTextField.isSecureTextEntry = true
+               }
+               
+               signUpAlert.addAction(saveAction)
+               signUpAlert.addAction(cancelAction)
+               
+               present(signUpAlert, animated: true, completion: nil)
+        
+        
+    }
+    
 }
