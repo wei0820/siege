90a5f802aee792634ccde2ba5fd4d90efbc8a823
Wei Wei <jheng.wei0919@gmail.com>
2020-11-18 14:10:53 +0800

    add view add vc

---
+++ siege/siege/SixCityListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SixCityListViewController.swift
+//  siege
+//
+//  Created by oneplay on 2020/11/18.
+//  Copyright © 2020 JackPan. All rights reserved.
+//
+
+import UIKit
+
+class SixCityListViewController: MGooogleAdsViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
